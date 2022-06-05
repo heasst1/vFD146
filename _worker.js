c725dd1c01c6134eabb2866b76c6141d2af45cce
heasst1 <106358288+heasst1@users.noreply.github.com>
2022-06-05 07:54:35 +0000

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('路径')) {
-      url.hostname = 'crimson-union-bf50.pimientwr.workers.dev'
+      url.hostname = 'zxvsfdhdhdtruet.gq'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
